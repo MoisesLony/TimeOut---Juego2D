8b012c8018c443d1b9cf4ebc8a7d6da477e7c489
Paul Bernal <82968269+PauloC21@users.noreply.github.com>
2021-08-20 12:22:46 -0600

    Respawn, Enemy, Transition
    Respawn por mejorar, Enemigo Provisional y transicion

---
+++ Juego2D/Assets/Scripts/PlayerRespawn.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerRespawn : MonoBehaviour
+{
+    private float cheackPointPX, cheackPointPY;
+
+    public Animator anim;
+    public GameObject transition;
+    void Start()
+    {
+        if (PlayerPrefs.GetFloat("cheackPointPX") != 0)
+        {
+            transform.position = (new Vector2(PlayerPrefs.GetFloat("cheackPointPX"), PlayerPrefs.GetFloat("cheackPointPY")));
+        }
+    }
+    public void reachedCheckPoint(float x, float y)
+    {
+        PlayerPrefs.SetFloat("cheackPointPX",x);
+        PlayerPrefs.SetFloat("cheackPointPX",y);
+    }
+
+    public void PlayerDamage()
+    {
+        anim.Play("PlayerHit");
+        transition.SetActive(true);      
+        Invoke("ReloadScene", 1);
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
